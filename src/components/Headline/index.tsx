fc04b323dcd9803f0fbac2c69003c2577e8729ab
efrencodes <efrencodes@gmail.com>
2023-10-19 00:00:41 -0600

    💄 Update UI

---
+++ src/components/Headline/index.tsx
@@ -0,0 +1,22 @@
+const HeadLine = () => {
+  return (
+    <>
+      <h1 className="text-neutral-900 text-xl font-semibold leading-[25px]">
+        Efrén Martínez
+      </h1>
+      <h2 className="text-gray-700 text-sm font-medium leading-[25px]">
+        <span className="text-teal-500">Desarrollador frontend</span>{" "}
+        especializado en <span className="text-amber-500">#javascript</span>.
+        Enfocado en desarrollo web con tecnologias como{" "}
+        <span className="text-green-500">#vuejs</span>,{" "}
+        <span className="text-cyan-500">#react</span> y{" "}
+        <span className="text-blue-500">#typescript</span>.
+      </h2>
+      <p className="text-neutral-900 text-xs font-semibold font-sans">
+        encuentráme en:
+      </p>
+    </>
+  );
+};
+
+export default HeadLine;
