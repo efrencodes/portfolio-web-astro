153fd8b8b65ea982b2c88d7c3718ce872d20cb26
Efren Martinez <efrencodes@gmail.com>
2023-10-20 18:16:53 +0000

    🎨 Improve structure

---
+++ src/config/postsList.ts
@@ -0,0 +1,9 @@
+export const postsList = [
+	{
+		title: 'Cómo convertir un número a formato de moneda en JavaScript',
+		description:
+			'El método Intl.NumberFormat() nos permite formatear a un valor más legible.',
+		image: 'https://res.cloudinary.com/efrencodes/image/upload/v1672193175/www.efrencodes.com/tecnologies/javascript.svg',
+		url: '/articulos/como-convertir-un-numero-a-formato-de-moneda-en-javascript'
+	}
+]
