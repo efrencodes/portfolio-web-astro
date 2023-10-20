a999d561fdfe0e43d805bc6677da069bf8f54ce3
Efren Martinez <efrencodes@gmail.com>
2023-10-20 18:52:49 +0000

    🔍️ Add config for SEO

---
+++ src/config/metatags.ts
@@ -0,0 +1,49 @@
+export interface metadataProps {
+	url: string
+	title: string
+	description: string
+	keywords: string
+	openGraph: {
+		url: string
+		siteName: string
+		images: {
+			url: string
+			width: string
+			height: string
+		}
+		locale: string
+		type: string
+		creator: string
+	}
+}
+
+export const metadataDefault: metadataProps = {
+	url: 'https://efrencodes.com',
+	title: 'Efrén Martínez - Frontend Engineer',
+	description:
+		'Frontend Engineer especializado en #javascript #vue #react #typescript #withastro #nextjs #cypress',
+	keywords:
+		'querétaro,tecnologia,software,web,desarrollo,fullstack,software engineer,developer,jamstack,vuejs,reactjs,méxico,typescript,tailwindcss,cypress,astro',
+	openGraph: {
+		url: 'https://efrencodes.com',
+		siteName: 'Efrén Martínez - Frontend Engineer',
+		images: {
+			url: 'https://res.cloudinary.com/efrencodes/image/upload/v1674800149/www.efrencodes.com/og-image-efrencodes.webp',
+			width: "1920",
+			height: "1080"
+		},
+		locale: 'es-MX',
+		type: 'summary_large_image',
+		creator: '@efrencodes'
+	}
+}
+
+export const structuredData = {
+	'@context': 'http://schema.org',
+	'@type': 'Person',
+	email: 'mailto:efrencodes@gmail.com',
+	image: '',
+	jobTitle: 'Frontend Engineer',
+	name: 'Efrén Martínez',
+	url: 'https://www.efrencodes.com/'
+}
