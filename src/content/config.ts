88ca879369e7a61ef29f62cef52c6f201e9bc58c
Efren Martinez <efrencodes@gmail.com>
2023-10-24 17:33:18 +0000

    ✨ Add funcionality for post

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.coerce.date(),
+        tags: z.array(z.string()).default(['others']),
+        authors: z.array(z.string()).default(['efrencodes']),
+        ogImage: z.string(),
+        cover: z.string(),
+        keywords: z.string(),
+        slug: z.string(),
+    })
+})
+
+export const collections = { blog };
