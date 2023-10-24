3335f6271a760f6dd0679ea5d8e2945e5c383738
Efren Martinez <efrencodes@gmail.com>
2023-10-24 23:22:45 +0000

    ✨ Add new posts

--- src/config/postsList.ts
+++ src/config/postsList.ts
@@ -8,4 +8,13 @@ export const postsList = [
       "https://res.cloudinary.com/efrencodes/image/upload/v1672193175/www.efrencodes.com/tecnologies/javascript.svg",
     url: "/articulos/como-convertir-un-numero-a-formato-de-moneda-en-javascript",
   },
+  {
+    id: "00002a",
+    title: "Maximiza el Poder de Astro: Dominando el Arte de los Slots",
+    description:
+      "Aprende a utilizar estos espacios reservados en tus componentes y potencia tu desarrollo web con Astro.",
+    image:
+      "https://res.cloudinary.com/efrencodes/image/upload/v1698187812/www.efrencodes.com/tecnologies/astro.svg",
+    url: "/articulos/dominando-el-arte-de-los-slots-en-astro",
+  },
 ];
