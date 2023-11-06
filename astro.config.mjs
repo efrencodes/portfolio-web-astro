aaf89c69b6dedb9676872f6dd133833e35f7706d
Efren Martinez <efrencodes@gmail.com>
2023-11-06 19:46:20 +0000

    🔍️  Update sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://efrencodes.com",
+  site: "https://www.efrencodes.com/",
   integrations: [
     tailwind(),
     mdx(),
     sitemap({
       changefreq: "weekly",
       priority: 0.7,
+      customPages: [
+        "https://www.efrencodes.com/articulos/como-convertir-un-numero-a-formato-de-moneda-en-javascript",
+        "https://www.efrencodes.com/articulos/dominando-el-arte-de-los-slots-en-astro",
+      ],
     }),
   ],
   output: "server",
