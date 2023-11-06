de1f8bf78df69d6aa0962de63bbc46b7a63da46f
Efren Martinez <efrencodes@gmail.com>
2023-11-06 19:15:12 +0000

    📈 Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://efrencodes.com',
-  integrations: [tailwind(), mdx(), sitemap({
-    changefreq: 'weekly',
-    priority: 0.7,
-  })]
+  site: "https://efrencodes.com",
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+    }),
+  ],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
