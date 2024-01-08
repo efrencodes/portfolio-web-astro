1b059a31c838c5490c046fb0411f4c524511fa44
Efren Martinez <efrencodes@gmail.com>
2024-01-08 05:41:56 +0000

    ✨ Sort posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,13 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        date: z.coerce.date(),
+        pubDate: z.date(),
         tags: z.array(z.string()).default(['others']),
         authors: z.array(z.string()).default(['efrencodes']),
         ogImage: z.string(),
         cover: z.string(),
         keywords: z.string(),
-        slug: z.string(),
-        order: z.number()
+        slug: z.string()
     })
 })
 
